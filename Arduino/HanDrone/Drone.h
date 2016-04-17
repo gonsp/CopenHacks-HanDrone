abe820d35d0c9d2d59f3df972bbb1cbbc3b0c288
Gonzalo Solera <gonzalo1796@gmail.com>
2016-04-17 02:58:25 +0200

    DRONE CONTROLLING WORKSgit add -Agit add -A!

--- Arduino/HanDrone/Drone.h
+++ Arduino/HanDrone/Drone.h
@@ -1,22 +1,23 @@
 #ifndef Drone_h
 #define Drone_h
 
-#define axis_z A3
-#define axis_y A0
-#define axis_x A1
-
-#define max 755
-
-#define default_axis_z max
-#define default_axis_y max/2
-#define default_axis_x max/2
+#include "LeapMotion.h"
 
 class Drone {
 private:
+    const int axis_z = 13;
+    const int axis_y = 11;
+    const int axis_x = 10;
+
+    const int max = 255;
+    const int default_axis_z = max;
+    const int default_axis_y = max/2;
+    const int defualt_axis_x = max/2;
 public:
     Drone();
-    void set_action(HandInfo handinfo);
-
+    //void set_action(HandInfo handinfo);
+    void sync();
+    void setAction(HandInfo handinfo);
 };
 
 #endif
--- Arduino/HanDrone/LeapMotion.h
+++ Arduino/HanDrone/LeapMotion.h
@@ -1,19 +1,24 @@
 #ifndef LeapMotion_h
 #define LeapMotion_h
 
+#include <Arduino.h>
+
 struct HandInfo {
     double axis_x;
     double axis_y;
     double axis_z;
+    bool cercle;
 };
 
 class LeapMotion {
 private:
+    char buffer[30];
     HandInfo last_handinfo;
+    char* readString();
 public:
     LeapMotion();
     bool refresh();
-    HandInfo getHandInfo();
+    HandInfo getHandInfo() const;
 };
 
 #endif
